590d740b7dde88731979074ca9e2a99cff7507f4
GioPa97 <giovannipagliara97@.com>
2023-01-10 17:16:51 +0100

    Corretto controller Pizza

--- La Mia Pizzeria 1/Controllers/PizzaController.cs
+++ La Mia Pizzeria 1/Controllers/PizzaController.cs
@@ -1,5 +1,6 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using La_Mia_Pizzeria_1.Database;
 using La_Mia_Pizzeria_1.Models;
+using Microsoft.AspNetCore.Mvc;
 
 
 namespace La_Mia_Pizzeria_1.Controllers
@@ -7,8 +8,79 @@ namespace La_Mia_Pizzeria_1.Controllers
     public class PizzaController : Controller
     {
 
-        
-       
+
+        public IActionResult Index()
+        {
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                List<Pizza> listaDellePizza = db.Pizzas.ToList<Pizza>();
+                return View("Index", listaDellePizza);
+            }
+
+        }
+
+        public IActionResult Details(int id)
+        {
+
+            /*
+            bool FunzioneDiRicercaPostById(Post post)
+            {
+                return post.Id == id;
+            }
+            */
+
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                // LINQ: syntax methos
+                Pizza pizzaTrovato = db.Pizzas
+                    .Where(SingolaPizzaNelDb => SingolaPizzaNelDb.Id == id)
+                    .FirstOrDefault();
+
+                // LINQ: query syntax
+                /* Post postTrovato =
+                     (from p in db.Posts
+                      where p.Id == id
+                      select p).FirstOrDefault<Post>();*/
+
+                // SQL QUERY
+                /* Post postTrovato =
+                     db.Posts.FromSql($"SELECT * FROM Posts WHERE Id = {id}")
+                     .FirstOrDefault<Post>(); */
+
+                if (pizzaTrovato != null)
+                {
+                    return View(pizzaTrovato);
+                }
+
+                return NotFound("la pizza con l'id cercato non esiste!");
+
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Pizza formData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", formData);
+            }
+
+            using (PizzeriaContext db = new PizzeriaContext())
+            {
+                db.Pizzas.Add(formData);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
 
     }
 }
